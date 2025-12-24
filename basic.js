a6914d5485db223ffe2fc432a7b9b14a1b9d0e48
MAAZALI99 <maazali4569@gmail.com>
2025-12-24 19:42:08 +0500

    Initial commit

---
+++ basic.js
@@ -0,0 +1,111 @@
+// VARIABLES/CONSTANT
+// Create a VARIABLE
+let myage = 15;
+
+console.log(myage);
+
+// Country name
+const country = "pakistan";
+
+console.log(country);
+
+// Change variable value
+let value = 10;
+value = 20; 
+
+console.log(value);
+
+// Addition of two numbers
+let addition = 50;
+addition = 50 + 10;
+console.log(addition);
+
+// String Combination
+let firstname = "Maaz";
+let lastname = "Ali";
+
+console.log(firstname + lastname);
+
+// Try changing const
+const val = 10;
+// val = 20;
+
+console.log(val);
+
+// DATA TYPES
+// Booolean Practise
+let isRaining = true;
+isRaining = false;
+console.log(isRaining);
+
+// Number Operation
+let num = 50;
+let num1 = 40;
+console.log(num * num1);
+
+// Template Literals 
+let age = 15;
+let city = "Lahore";
+console.log("I am " + [age] + " Years old and I live in " +[city]);
+
+// Type conversion
+let string = "25";
+string = "25" * 5;
+console.log(string);
+
+// Arrays
+let myarray = [ "Watermelon" , "Strawberry" , "Apple" , "Orange" , "Pineapple"];
+console.log(myarray);
+
+// Access First Element
+
+console.log(myarray[0]);
+
+// Access Last Element
+
+console.log(myarray[4]);
+
+// replace an element
+
+myarray[2] = "Kiwi"
+
+// Add a six fruit
+
+myarray[6] = "Banana"
+
+// Mixed Data Array
+
+let newarray = ["SYED MAAZ ALI" , 15 , undefined , true];
+console.log(newarray);
+
+// Access Middle Item 
+
+console.log(newarray[1]);
+
+// Numbers Array
+
+let numarray = [10 , 20 , 30 , 40 , 50];
+console.log(numarray[0]);
+console.log(numarray[2]);
+
+// Update Two Elements
+
+let colarray = ["Red" , "blue" , "Grey" , "Yellow" , "Brown"];
+colarray[0] = "Aqua"
+colarray[3] = "Green"
+console.log(colarray);
+
+// Array of Names
+
+let namearray = ["Maaz" , "Abu Huraira" , "Dawood"];
+console.log(namearray[1]);
+
+// Functions
+// Simple Functions
+
+function printHello() {
+  console.log("Hello World");
+}
+
+// Call the function
+printHello();
